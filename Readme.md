3c26fe99aeea6d6ca940e20e73e5a3f8eb9380ed
Rodion Chachura <geekrodion@gmail.com>
2019-08-03 16:07:47 +0300

    feature: html, css and general utils

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# snake-game
+
+>
+
+![all text](https://cdn-images-1.medium.com/max/800/1*GtyZdI2hERX1azgtEpKazA.gif)
+
+## [Play](https://rodionchachura.github.io/snake-game/)
+
+## License
+
+MIT © [RodionChachura](https://geekrodion.com)
---
+++ main.js
@@ -0,0 +1,22 @@
+// #region general utils
+const getRange = length => [...Array(length).keys()]
+const getRandomFrom = array => array[Math.floor(Math.random() * array.length)]
+const getWithoutLastElement = array => array.slice(0, array.length - 1)
+const getLastElement = array => array[array.length - 1]
+const areEqual = (one, another) => Math.abs(one - another) < 0.000000000001
+// #endregion
+
+// #region geometry
+// #endregion
+
+// #region constants
+// #endregion
+
+// #region game core
+// #endregion
+
+// #region rendering
+// #endregion
+
+// #region main
+// #endregion
