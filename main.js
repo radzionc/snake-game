dea212fe609e5fb6b586357158bd89de0d48562c
Rodion Chachura <geekrodion@gmail.com>
2019-08-15 06:19:58 +0300

    feature: game loop

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
+// #region constants
+const UPDATE_EVERY = 1000 / 60
+// #endregion
+
+// #region rendering
+const render = (state) => {
+  console.log('render')
+}
+// #endregion
+
 // #region main
+const getInitialState = () => {
+  return {}
+}
+
+const getNewStatePropsOnTick = (props) => {
+  console.log('get new state')
+}
+
 const startGame = () => {
-  console.log('Start Game')
+  let state = getInitialState()
+  const updateState = props => {
+    state = { ...state, ...props }
+  }
+
+  window.addEventListener('resize', () => {
+    console.log('resize')
+  })
+  window.addEventListener('keydown', ({ which }) => {
+    console.log('keydown: ', which)
+  })
+  window.addEventListener('keyup', ({ which }) => {
+    console.log('keyup: ', which)
+  })
+
+  const tick = () => {
+    const newProps = getNewStatePropsOnTick(state)
+    updateState(newProps)
+    render(state)
+  }
+  setInterval(tick, UPDATE_EVERY)
 }
 // #endregion
 
